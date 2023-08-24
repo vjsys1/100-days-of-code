377ab39e97ed3180932eb01eed87594f9fac2154
vjsys1 <vijay.20jics069@jietjodhpur.ac.in>
2023-08-25 01:29:40 +0530

    pattern diamond

--- pattern.cpp
+++ pattern.cpp
@@ -45,7 +45,42 @@ using namespace std;
             cout<<endl;
         }
     }
-
+    void print2(int n) {
+        
+        // code here
+        for(int i =0 ;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++){
+                cout<< " ";
+            }
+            for(int j=0;j<i+1;j++){
+                cout<< '*';
+                cout<<" ";
+                
+            }
+            for(int j=0;j<n-i-1;j++){
+                cout<< " ";
+            }
+            cout<<endl;
+            
+        }
+         for(int i =0 ;i<n;i++)
+        {
+            for(int j=0;j<i;j++){
+                cout<< " ";
+            }
+            for(int j=0;j<n-i;j++){
+                cout<< '*';
+                cout<<" ";
+            }
+            for(int j=0;j<n-i;j++){
+                cout<< " ";
+            }
+            cout<<endl;
+            
+        }
+        
+    }
 //{ Driver Code Starts.
 
 int main() {
