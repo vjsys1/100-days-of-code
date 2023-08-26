7b68532a3103a18a434cc720537e70b73878d4bf
vjsys1 <vijay.20jics069@jietjodhpur.ac.in>
2023-08-26 23:37:35 +0530

    new pattern

---
+++ day 2/stl.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+#include<paths.h>
+
+
+
+using namespace std;
+
+void explainPair(){
+    pair <int,int> p = {1,3};
+    cout<<p.first;
+}
+int main()
+{
+    explainPair();
+    return 0 ;
+}
